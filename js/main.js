5fed3e28ac7bbe35dfcaa188ece64d684d99e756
ssoonD <ksj96226@gmail.com>
2020-10-13 19:58:16 +0900

    현재 활성시킬 씬 결정하기

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 /*즉시 호출 함수 => 함수가 자동으로 호출
 사용 이유 : 전역 변수 사용을 피하기 위해서*/
 (() => {
+
+    let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+    let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 높이값의 합
+    let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+
     const sceneInfo = [{
             // 0
             type: 'sticky',
@@ -46,8 +51,31 @@
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
         }
     }
+
+    function scrollLoop() {
+        prevScrollHeight = 0;
+        // 현재 눈앞에 몇 번째 스크롤 섹션이 스크롤 중인지를 판별 
+        for (let i = 0; i < currentScene; i++) {
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+
+        if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            currentScene++;
+        }
+        if (yOffset < prevScrollHeight) {
+            if (currentScene === 0) return; // 브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지(모바일)
+            currentScene--;
+        }
+
+        console.log(currentScene);
+    }
+
     // 윈도우 창의 사이즈가 변할 때 같이 반응하도록 
     window.addEventListener('resize', setLayout);
+    window.addEventListener('scroll', () => {
+        yOffset = window.pageYOffset;
+        scrollLoop();
+    })
 
     setLayout();
 })();
